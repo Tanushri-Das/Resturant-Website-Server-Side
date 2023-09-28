ed0a76533c4219ae1aa3c6b73e6754db59c04215
tanu <dastanushri402@gmail.com>
2023-09-28 09:52:51 +0600

    add review and contact details in mongodb

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const express=require('express');
-const app=express();
-const cors=require('cors');
-require('dotenv').config()
-const port=process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
-
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tdjlbxg.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,7 +38,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,47 +46,288 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const menuCollection=client.db('ResturantWebsite').collection('menu');
-    const reviewCollection=client.db('ResturantWebsite').collection('reviews');
-    const cartCollection=client.db('ResturantWebsite').collection('carts');
+    const menuCollection = client.db("ResturantWebsite").collection("menu");
+    const reviewCollection = client.db("ResturantWebsite").collection("reviews");
+    const cartCollection = client.db("ResturantWebsite").collection("carts");
+    const usersCollection = client.db("ResturantWebsite").collection("users");
+    const paymentCollection = client.db("ResturantWebsite").collection("payments");
+    const contactCollection = client.db("ResturantWebsite").collection("contact");
 
-    app.get('/menu',async(req,res)=>{
-        const result=await menuCollection.find().toArray();
-        res.send(result);
-    })
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
-    app.get('/reviews',async(req,res)=>{
-        const result=await reviewCollection.find().toArray();
-        res.send(result);
-    })
+    // warning : use verifyJWT before using verifyAdmin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+      next();
+    };
+    /**
+     * do not show secure links to those who should not see the links
+     * use jwt token:verifyJWT
+     * use verifyAdmin middleware
+     */
 
+    // users related apis
 
-    // cart collection
-    
-    app.post('/carts',async(req,res)=>{
-      const item=req.body;
-      console.log(item);
-      const result=await cartCollection.insertOne(item);
+    app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
       res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      console.log("existingUser", existingUser);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // security layer : verifyJWT
+    // email same
+    // check admin
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // menu related apis
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/menu", verifyJWT, verifyAdmin, async (req, res) => {
+      const newItem = req.body;
+      const result = await menuCollection.insertOne(newItem);
+      res.send(result);
+    });
+
+    app.delete("/menu/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // reviews related apis
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/reviews", verifyJWT, async (req, res) => {
+      const newItem = req.body;
+      const result = await reviewCollection.insertOne(newItem);
+      res.send(result);
+    });
+
+    // contact
+
+    app.post("/contact", verifyJWT, async (req, res) => {
+      const newItem = req.body;
+      const result = await contactCollection.insertOne(newItem);
+      res.send(result);
+    });
+
+    // cart collection apis
+
+    app.get("/carts", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // create payment
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+      // console.log(price,amount)
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    // payment related api
+    app.post("/payments", verifyJWT, async (req, res) => {
+      const payment = req.body;
+      const insertResult = await paymentCollection.insertOne(payment);
+      const query = {
+        _id: { $in: payment.cartItems.map((id) => new ObjectId(id)) },
+      };
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({ insertResult, deleteResult });
+    });
+
+    app.get("/payments",verifyJWT, async (req, res) => {
+      const email = req.query.email;
+    
+      if (!email) {
+        return res.status(400).send({ error: true, message: "Email is required" });
+      }
+    
+      try {
+        const paymentDetails = await paymentCollection.find({ email: email }).toArray();
+        // const paymentCount = paymentDetails.length;
+    
+        // res.json({ paymentCount, paymentDetails });
+        res.json(paymentDetails);
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send({ error: true, message: "An error occurred" });
+      }
+    });
+
+    app.get("/admin-stats", verifyJWT, verifyAdmin, async (req, res) => {
+      const users = await usersCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+      // best way to get sum of a field is to sum group and sum operator
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce((sum, payment) => sum + payment.price, 0);
+      res.send({ users, menuItems, orders, revenue });
+    });
+
+    app.get("/user-stats",verifyJWT, async (req, res) => {
+      const email = req.query.email;
+    
+      if (!email) {
+        return res.status(400).send({ error: true, message: "Email is required" });
+      }
+    
+      try {
+        const menuItems = await menuCollection.estimatedDocumentCount();
+        const paymentCount = await paymentCollection.countDocuments({ email: email });
+        const orderCount = await cartCollection.countDocuments({ email: email });
+        const reviewCount = await reviewCollection.countDocuments({ email: email });
+        res.json({ payment: paymentCount ,orders:orderCount,reviews:reviewCount,menu:menuItems});
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send({ error: true, message: "An error occurred" });
+      }
+    });
+    
+    app.get('/order-stats', verifyJWT, verifyAdmin, async(req, res) =>{
+      const pipeline = [
+        {
+          $lookup: {
+            from: 'menu',
+            localField: 'menuItems',
+            foreignField: '_id',
+            as: 'menuItemsData'
+          }
+        },
+        {
+          $unwind: '$menuItemsData'
+        },
+        {
+          $group: {
+            _id: '$menuItemsData.category',
+            count: { $sum: 1 },
+            total: { $sum: '$menuItemsData.price' }
+          }
+        },
+        {
+          $project: {
+            category: '$_id',
+            count: 1,
+            total: { $round: ['$total', 2] },
+            _id: 0
+          }
+        }
+      ];
+
+      const result = await paymentCollection.aggregate(pipeline).toArray()
+      console.log(result)
+      res.send(result)
+
     })
-
-
-
-
-
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-
   }
 }
 run().catch(console.dir);
 
-
-app.get('/',(req,res)=>{
-    res.send('Resturant server side is running');
-})
-app.listen(port,()=>{
-    console.log(`Resturant website server side running on port ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("Resturant server side is running");
+});
+app.listen(port, () => {
+  console.log(`Resturant website server side running on port ${port}`);
+});
